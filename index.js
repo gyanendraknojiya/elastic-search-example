a4cf4a7e29c9450e93ad5e5cc5f80c96b57b5e15
Gyanendra Knojiya <gyanendrak064@gmail.com>
2021-11-13 13:17:16 +0530

    adding elastic search

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Notes = require("./models/notes");
 
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({ node: "http://localhost:9200" });
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -10,9 +13,13 @@ app.use(express.urlencoded({ extended: false }));
 let PORT = process.env.PORT || 8080;
 
 app.get("/", async (req, res, next) => {
-  const response = await Notes.find();
+  const result = await client.search({
+    index: "my-index",
+    body: {},
+  });
+
   return res.status(200).json({
-    response,
+    result,
   });
 });
 
@@ -22,26 +29,30 @@ app.post("/add", async (req, res, next) => {
     title,
     body,
   });
+
+  const d = await client.index({
+    index: "my-index",
+    body: {
+      title,
+      body,
+    },
+  });
+
   return res.status(201).json({
-    response,
+    d,
   });
 });
 
-mongoose.connect(
-  process.env.MONGO_URL,
-  { useNewUrlParser: true },
-);
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true });
 
 const db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", console.error.bind(console, "connection error:"));
 
-db.once('open', function () {
-  console.log('connected to the database');
+db.once("open", function () {
+  console.log("connected to the database");
 });
 
-      app.listen(PORT, () => {
-        console.log("server is running at port: " + PORT);
-      })
-
-
+app.listen(PORT, () => {
+  console.log("server is running at port: " + PORT);
+});
